2e4df2bfbbae6e0d2dae6726c331fc60be091abe
Azat Serikov <azatsdev@gmail.com>
2024-02-25 15:01:24 +0600

    feat: untrusted escrow

---
+++ src/1/UntrustedEscrow.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENCED
+pragma solidity 0.8.21;
+
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "forge-std/console.sol";
+
+contract UntrustedEscrow {
+    using SafeERC20 for IERC20;
+
+    struct Escrow {
+        bytes32 id;
+        address from;
+        address to;
+        address token;
+        uint256 amount;
+        uint256 timestamp;
+        uint256 timelock;
+    }
+
+    mapping(bytes32 => Escrow) private escrows;
+
+    function getEscrow(address from, address to, address token) public view returns(Escrow memory escrow) {
+        bytes32 escrowId = keccak256(abi.encodePacked(from, to, token)); 
+        escrow = escrows[escrowId];
+    }
+
+    function pay(address to, address token, uint256 amount, uint256 timelock) external {
+        require(to != address(0), "Invalid recipient");
+        require(token != address(0), "Invalid token");
+        require(amount > 0, "Amount must be non-zero");
+        require(timelock > 0, "Timelock must be non-zero");
+
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+
+        bytes32 escrowId = keccak256(abi.encodePacked(msg.sender, to, token));
+
+        escrows[escrowId] = Escrow({
+            id: escrowId,
+            from: msg.sender,
+            to: to,
+            token: token,
+            amount: amount,
+            timestamp: block.timestamp,
+            timelock: timelock
+        });
+    }
+
+    function withdraw(address from, address token) external {
+        bytes32 escrowId = keccak256(abi.encodePacked(from, msg.sender, token));
+
+        Escrow memory escrow = getEscrow(from, msg.sender, token);
+        require(escrow.id == escrowId, "Escrow does not exist or withdrawn");
+        require(escrow.timestamp + escrow.timelock >= block.timestamp, "Too early to withdraw");
+        delete escrows[escrow.id];
+
+        IERC20(escrow.token).safeTransfer(msg.sender, escrow.amount);
+    } 
+}
---
+++ test/1/UntrustedEscrow.t.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import "forge-std/console.sol";
+import {UntrustedEscrow} from "src/1/UntrustedEscrow.sol";
+import {TokenBannable} from "src/1/TokenBannable.sol";
+
+contract UntrustedEscrowTest is Test {
+    UntrustedEscrow escrow;
+    TokenBannable token;
+
+    address admin = vm.addr(0x1);
+    address alice = vm.addr(0x2);
+    address bob = vm.addr(0x3);
+    address charlie = vm.addr(0x4);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function setUp() public {
+        escrow = new UntrustedEscrow();
+        token = new TokenBannable(admin, "Token", "TKN");
+        
+        vm.prank(admin);
+        token.mint(alice, 100 ether);
+    }
+
+    function test_Pay() public {
+        assertEq(token.balanceOf(alice), 100 ether);
+
+        vm.startPrank(alice);
+        vm.expectEmit();
+        emit Approval(alice, address(escrow), 1 ether);
+        token.approve(address(escrow), 1 ether);
+        emit Transfer(alice, address(escrow), 1 ether);
+        escrow.pay(bob, address(token), 1 ether, 3 days);
+
+        assertEq(token.balanceOf(alice), 99 ether);
+        assertEq(token.balanceOf(address(escrow)), 1 ether);
+        assertEq(token.balanceOf(bob), 0 ether);
+
+        UntrustedEscrow.Escrow memory _escrow = escrow.getEscrow(alice, bob, address(token));
+        assertEq(_escrow.id, keccak256(abi.encodePacked(alice, bob, token)));
+        assertEq(_escrow.from, alice);
+        assertEq(_escrow.to, bob);
+        assertEq(_escrow.token, address(token));
+        assertEq(_escrow.amount, 1 ether);
+        assertEq(_escrow.timestamp, block.timestamp);
+        assertEq(_escrow.timelock, 3 days);
+
+        vm.stopPrank();
+    }
+
+    function test_Withdraw() public {
+        test_Pay();
+
+        skip(3 days);
+
+        vm.prank(bob);
+        escrow.withdraw(alice, address(token));
+
+        assertEq(token.balanceOf(alice), 99 ether);
+        assertEq(token.balanceOf(address(escrow)), 0);
+        assertEq(token.balanceOf(bob), 1 ether);
+    }
+}
